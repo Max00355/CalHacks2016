d90c62314604aad72a3421ad5fff790331d4a320
Frankie Primerano <frankieprimerano@gmail.com>
2016-11-11 23:15:09 -0800

    Getting flights works

--- utils/getFlights.py
+++ utils/getFlights.py
@@ -1,5 +1,12 @@
+import amadeus
+
 API_KEY = "ikjnlVEIoDIonA0RaGak9TGmgTKGhhcv"
 
-
-def getFlight(outOf, to, depart):
-    pass
+def getFlights(outOf, to, leaves, returns=None):
+    flight = amadeus.Flights("ikjnlVEIoDIonA0RaGak9TGmgTKGhhcv")
+    return flight.low_fare_search(
+        origin=outOf,
+        destination=to,
+        departure_date=leaves + "" if returns is None else "--{}".format(returns)
+    )
+    
