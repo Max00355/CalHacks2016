b45b3cf67cc4a24f71c3e3cdf08ed3edee36f7ed
Nicholas Swift <nicholas.w.swift@gmail.com>
2016-11-12 16:39:02 -0800

    Date merge conflict fixed

--- utils/convertDate.py
+++ utils/convertDate.py
@@ -2,17 +2,11 @@ from dateutil import parser
 import toDate
 import datetime
 def convertDate(date):
-<<<<<<< HEAD
     try:
         date = parser.parse(date)
     except:
         return None
-=======
-
-    print(date)
-
-    date = parser.parse(date)
->>>>>>> 2214723d49742263b1243bd8c0050e74f3df9f43
+        
     if (date - datetime.datetime.now()).days < 0:
         increment = True
     else:
